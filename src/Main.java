ff43e13048bdab7a3dc80877d0cd899c75ac9377
Reinaldo Barbosa <reinaldobarbosa034@gmail.com>
2023-08-08 20:51:11 -0300

    finalizei a classes main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
 public class Main {
     public static void main(String[] args) {
 
-        //Pessoas
-        Pessoa pessoa1 = new Pessoa("Cirilo", "2878991887", "928739ui","1199887278", "1192761221","Rua Carldos dumont");
-        Pessoa pessoa2 = new Pessoa("Maria joaquina", "3129913387", "9i8898ui","1192761221", "1199887278","Rua Carldos dumont");
+        //Criando Objetos
+
+        //Pessoa
+        Pessoa pessoa1 = new Pessoa();
+        Pessoa pessoa2 = new Pessoa();
 
         //Piloto
-        Piloto piloto1 = new Piloto("Kleiton","039874874","22");
+        Piloto piloto1 = new Piloto();
+
+        //Balao
+        Balao balao1 = new Balao();
+
+        //Atribuindo Valores
+
+        //Pessoa1
+
+        pessoa1.setNome("Cirilo");
+        pessoa1.setCpf("2878991887");
+        pessoa1.setRG("928739ui");
+        pessoa1.setTelefone("1199887278");
+        pessoa1.setTelContado("1192761221");
+        pessoa1.setEndereço("Rua Carldos dumont");
+
+        System.out.println(pessoa1.getNome());
+
+        //Pessoa 2
+
+        pessoa2.setNome("Maria Joaquina");
+        pessoa2.setCpf("29836702791");
+        pessoa2.setRG("2328127ty");
+        pessoa2.setTelefone("11942324050");
+        pessoa2.setTelContado("11995578798");
+        pessoa2.setEndereço("Rua Carldos dumont");
+
+        System.out.println(pessoa2.getNome());
+
+        //Piloto
+
+        piloto1.setNome("Kleiton");
+        piloto1.setCpf("00999888765");
+        piloto1.setNumeroRegistro("22");
+
+        System.out.println(piloto1.getNome());
 
         //Balão
 
-        Balao balao1 = new Balao("Caça", "312");
+        balao1.setIndeti("Caça");
+        balao1.setRegistro("321");
+
+        System.out.println(balao1);
     }
 }
